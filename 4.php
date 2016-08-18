5396411686b4ce8a5b4a95957e3fae33912f537f
pnechushkin <pnechushkin@gmail.com>
2016-08-18 13:36:12 +0300

    4

---
+++ 4.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+$dir=__DIR__.'/4';
+echo $dir;
+    function showFile($dir)
+    {
+
+        if ($papka = scandir($dir)) {
+            echo 'Список папки: '.$dir.'<br/>';
+            natcasesort($papka);
+            foreach ($papka as $file) {
+                echo $file.'<br/>';
+            }
+            }
+
+        }
+
+showFile($dir);
