6b62fd2e25a43ade85ef7b0090cc03ea04a6fcee
pnechushkin <pnechushkin@gmail.com>
2016-08-22 16:31:34 +0300

    11/1

--- 11.php
+++ 11.php
@@ -1,13 +1,32 @@
 <?php
 $string = 'а Васька слушает да ест. а воз и ныне там. а вы друзья как ни садитесь, все в музыканты не годитесь. а король-то — голый. а ларчик просто открывался.а там хоть трава не расти.';
 echo $string.'<br/>';
+
+if (!function_exists('mb_ucfirst') && extension_loaded('mbstring'))
+{
+    /**
+     * mb_ucfirst - преобразует первый символ в верхний регистр
+     * @param string $str - строка
+     * @param string $encoding - кодировка, по-умолчанию UTF-8
+     * @return string
+     */
+    function mb_ucfirst($str, $encoding='UTF-8')
+    {
+        $str = mb_ereg_replace('^[\ ]+', '', $str);
+        $str = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding).
+            mb_substr($str, 1, mb_strlen($str), $encoding);
+        return $str;
+    }
+}
+
+
 function relsrt ($string){
-$arr=preg_split("/[\.]+$/", $string);
-print_r($string);
+$arr=preg_split("/[\.]+/", $string);
+
 $stroka=null;
 foreach ($arr as $value)
-{echo $value.'<br/>';
-    if (!empty($value)) {$stroka.= $value.'. ';}
+{
+    if (!empty($value)) {$stroka.= mb_ucfirst($value).'. ';}
 }
 return $stroka;
 }
